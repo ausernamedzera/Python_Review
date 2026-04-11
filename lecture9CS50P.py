887d259332ef3b5c497b4858c93491241ac8aea2
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-04-11 23:28:54 +0300

    lecture9_temporary_notes_1

--- lecture9CS50P.py
+++ lecture9CS50P.py
@@ -1,12 +1,12 @@
 def main():
-    student = get_student()
+    student = get_student() #return değerine göre otomatik olarak dictionary'e dönecek
     print(f"{student['name']} from {student['house']}")
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return {"name": name, "house": house}
+    return {"name": name, "house": house} #zaten dictionary döndüreceği için, return değeri dictionary olacak
 
 
 if __name__ == "__main__":
