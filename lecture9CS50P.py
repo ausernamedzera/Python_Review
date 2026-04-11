53beb5e15ad57c395b14e77b42686317e64802ce
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-04-11 23:32:32 +0300

    lecture9_temporary_notes_1_Classes

--- lecture9CS50P.py
+++ lecture9CS50P.py
@@ -1,6 +1,6 @@
 def main():
     student = get_student() #return değerine göre otomatik olarak dictionary'e dönecek
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student['name']} from {student['house']}") #dictionaries are mutable
 
 
 def get_student():
@@ -8,6 +8,10 @@ def get_student():
     house = input("House: ")
     return {"name": name, "house": house} #zaten dictionary döndüreceği için, return değeri dictionary olacak
 
+
+
+###CLASSES###
+#you can create your own data type
 
 if __name__ == "__main__":
     main()
