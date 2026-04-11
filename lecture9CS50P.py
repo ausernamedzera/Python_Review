cd795439a11240502a1cad72d839d1ad26f1cbde
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-04-11 23:34:42 +0300

    lecture9_temporary_notes_1_Classes_v1

--- lecture9CS50P.py
+++ lecture9CS50P.py
@@ -1,13 +1,19 @@
+class Student:
+    ...
+
+
 def main():
     student = get_student() #return değerine göre otomatik olarak dictionary'e dönecek
     print(f"{student['name']} from {student['house']}") #dictionaries are mutable
 
 
 def get_student():
+    """""
     name = input("Name: ")
     house = input("House: ")
     return {"name": name, "house": house} #zaten dictionary döndüreceği için, return değeri dictionary olacak
-
+"""
+    student = Student() #classes have their "Attributes"
 
 
 ###CLASSES###
