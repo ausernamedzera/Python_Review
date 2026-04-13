2f35e6c7acc4e688e4c4d1c897857902f04140ac
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-04-13 23:15:43 +0300

    Character_Weapons

--- OOP_MiniGame.py
+++ OOP_MiniGame.py
@@ -2,4 +2,7 @@ class Character:
     def __init__(self, c_name, c_health, weapons):
         self.name = c_name
         self.health = c_health
-        self.weapons = weapons
+
+class Weapons:
+    def __init__(self):
+        self.weapons = []
