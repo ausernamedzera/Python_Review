feeeb3fbecf0174c0f25298653c7a12ebddf857a
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-04-13 23:43:17 +0300

    Character_Class_methods

--- OOP_MiniGame.py
+++ OOP_MiniGame.py
@@ -3,3 +3,12 @@ class Character:
         self.name = name
         self.health = health
         self.a_power = attack_power
+
+    def introduce(self):
+        print(f"Hello {self.name}, your current health is {self.health}. and your attack power is {self.a_power}")
+
+    def take_damage(self, damage):
+        self.health -= damage
+
+    def is_alive(self):
+        return self.health > 0
