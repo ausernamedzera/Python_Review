b99794ee6686bd7de01e80385b0155fe09417be2
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-04-15 23:55:05 +0300

    inheritance_warrior_override_and_testing

--- OOP_MiniGame.py
+++ OOP_MiniGame.py
@@ -20,6 +20,9 @@ class Warrior(Character): #inherited
         #Character.__init__(self, w_name, w_health, w_attack_power)
         self.armor = armor
 
+    def introduce(self):
+        print(f"hello I am a warrior! my name is {self.name}, my health {self.health}, my attack power {self.a_power} and my  armor def {self.armor}")
+
 class Mage(Character):
     pass
 
@@ -27,5 +30,8 @@ def get_character():
     name = input("Enter your hero name: ")
     health = 100 #I want them to automatically set a default.
     attack_power = 25
-    return Character(name, health, attack_power)
+    armor = 200
+    return Warrior(name, health, attack_power, armor)
 
+hero = get_character()
+hero.introduce()
